486d60e51513a7e3ca87f9611aaf4166638661b9
bhaveshpandey <pandeybhavesh@gmail.com>
2021-11-05 17:06:45 +0000

    added comments in gollum_config.rb showing how to serve this wiki.

--- gollum_config.rb
+++ gollum_config.rb
@@ -1,1 +1,5 @@
+# serve this site using gollum as follows
+# NOTE: execute following from the root of the repo
+# gollum -c ./gollum_config.rb --ref gh-pages
+
 Precious::App.set(:wiki_options, index_page:"wiki/index.md")
